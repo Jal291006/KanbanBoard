e313e2c83c7dcbd9434a242e8cd7766d3a03098b
Jal Patel <jkp291006@gmail.com>
2026-06-04 22:19:10 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -581,7 +581,7 @@ function App() {
               <path strokeLinecap="round" strokeLinejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
             </svg>
           </div>
-          <h1 className="text-xl font-bold tracking-tight text-white">Project Alpha</h1>
+          <h1 className="text-xl font-bold tracking-tight text-white">Kanban Board</h1>
         </div>
         
         {/* Theme Picker & Logout */}
